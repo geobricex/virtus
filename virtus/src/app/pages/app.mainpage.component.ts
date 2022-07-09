993b84931a72ee7f724d3747e60087c66968060f
geobricex <69307530+geobricex@users.noreply.github.com>
2022-07-09 01:06:15 -0500

    improvements
    Logo colores, mejoras en inicio

--- virtus/src/app/pages/app.mainpage.component.ts
+++ virtus/src/app/pages/app.mainpage.component.ts
@@ -12,10 +12,7 @@ export class AppMainpageComponent {
  // cssUrl: string;
   constructor( private  _CargarScriptsService:CargarScriptsService /*public sanitizer: DomSanitizer*/) {
    // this.cssUrl = './app.pagemain.component.css';
-    _CargarScriptsService.loadingService(["https://res.cloudinary.com/bricex/raw/upload/v1657340119/library/io35k5oiz9vltw6cohvf.js",
-      "https://code.jquery.com/jquery-3.4.1.slim.min.js",
-    "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
-    "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"]);
+    _CargarScriptsService.loadingService(["https://res.cloudinary.com/bricex/raw/upload/v1657340119/library/io35k5oiz9vltw6cohvf.js"]);
 
   }
 
