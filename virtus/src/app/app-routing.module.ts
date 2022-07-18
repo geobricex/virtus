21cfa0f84ebe768ad2ba3551e45f4a2ee7bf1e61
Dúval Carvajal <duval.carvajal2017@uteq.edu.ec>
2022-07-17 19:32:22 -0500

    LOGIN
    interfaces login, registrase, recuperar cuenta e ingresar el codigo de verificacion

--- virtus/src/app/app-routing.module.ts
+++ virtus/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
 
 import {DashboardDemoComponent} from "./pages/dashboarddemo.component";
-import { CursosComponent } from './pages/cursos/cursos.component';
+import {CursosComponent} from './pages/cursos/cursos.component';
 
 import {AppLoginComponent} from './pages/app.login.component';
 import {AppMainpageComponent} from "./pages/app.mainpage.component";
@@ -10,26 +10,29 @@ import {AppMainComponent} from "./app.main.component";
 import {AppNotfoundComponent} from "./pages/app.notfound.component";
 import {MiscursosComponent} from "./pages/miscursos/miscursos.component";
 import {TemasComponent} from "./pages/temas/temas.component";
+import {SingupComponent} from "./pages/singup/singup.component";
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot([
-            {path: '', component: AppMainpageComponent},
-            {path: 'home', component: AppMainpageComponent},
-            {path: 'login', component: AppLoginComponent},
-            {path: 'app', component: AppMainComponent,
-              children: [
-                {path: '', component: DashboardDemoComponent},
-                {path: 'course', component: CursosComponent},
-                {path: 'mycourse', component: MiscursosComponent},
-                {path: 'mycourse/themes', component: TemasComponent}
-                ]
-            },
-            {path: 'login', component: AppLoginComponent},
-            {path: '**', component: AppNotfoundComponent},
-        ], {scrollPositionRestoration: 'enabled'})
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([
+      {path: '', component: AppMainpageComponent},
+      {path: 'home', component: AppMainpageComponent},
+      {path: 'login', component: AppLoginComponent},
+      {path: 'singup', component: SingupComponent},
+      {
+        path: 'app', component: AppMainComponent,
+        children: [
+          {path: '', component: DashboardDemoComponent},
+          {path: 'course', component: CursosComponent},
+          {path: 'mycourse', component: MiscursosComponent},
+          {path: 'mycourse/themes', component: TemasComponent}
+        ]
+      },
+      {path: 'login', component: AppLoginComponent},
+      {path: '**', component: AppNotfoundComponent},
+    ], {scrollPositionRestoration: 'enabled'})
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
