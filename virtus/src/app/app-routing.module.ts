e0e2db61ed9d096af368231051c01cbcf1de06fe
ANTHONY PACHAY <anthony.pachay2017@uteq.edu.ec>
2022-07-21 21:45:04 -0500

    CMB

--- virtus/src/app/app-routing.module.ts
+++ virtus/src/app/app-routing.module.ts
@@ -1,29 +1,32 @@
-import {RouterModule} from '@angular/router';
-import {NgModule} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-import {DashboardDemoComponent} from "./pages/dashboarddemo.component";
+import { DashboardDemoComponent } from "./pages/dashboarddemo.component";
 import { CursosComponent } from './pages/cursos/cursos.component';
 
-import {AppLoginComponent} from './pages/app.login.component';
-import {AppMainpageComponent} from "./pages/app.mainpage.component";
-import {AppMainComponent} from "./app.main.component";
-import {AppNotfoundComponent} from "./pages/app.notfound.component";
+import { AppLoginComponent } from './pages/app.login.component';
+import { AppMainpageComponent } from "./pages/app.mainpage.component";
+import { AppMainComponent } from "./app.main.component";
+import { AppNotfoundComponent } from "./pages/app.notfound.component";
+import { TestvoiceComponent } from './testvoice/testvoice.component'
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
-            {path: '', component: AppMainpageComponent},
-            {path: 'home', component: AppMainpageComponent},
-            {path: 'login', component: AppLoginComponent},
-            {path: 'app', component: AppMainComponent,
-              children: [
-                {path: '', component: DashboardDemoComponent},
-                {path: 'course', component: CursosComponent}
+            { path: '', component: AppMainpageComponent },
+            { path: 'home', component: AppMainpageComponent },
+            { path: 'login', component: AppLoginComponent },
+            {
+                path: 'app', component: AppMainComponent,
+                children: [
+                    { path: '', component: DashboardDemoComponent },
+                    { path: 'course', component: CursosComponent }
                 ]
             },
-            {path: 'login', component: AppLoginComponent},
-            {path: '**', component: AppNotfoundComponent},
-        ], {scrollPositionRestoration: 'enabled'})
+            { path: 'login', component: AppLoginComponent },
+            { path: 'testvoice', component: TestvoiceComponent },
+            { path: '**', component: AppNotfoundComponent },
+        ], { scrollPositionRestoration: 'enabled' })
     ],
     exports: [RouterModule]
 })
