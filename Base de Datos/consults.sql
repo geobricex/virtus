13c73618f8acdb21a4b57e6bb0e4695ca96311ba
geobricex <69307530+geobricex@users.noreply.github.com>
2022-08-05 15:00:53 -0500

    .ignore

--- Base de Datos/consults.sql
+++ Base de Datos/consults.sql
@@ -1,12 +1,12 @@
 select * from persons;
 
---delete from persons where name_person = 'Juan';
+-- delete from persons where name_person = 'Juan';
 select * from utils;
 
 select id_syllabu, name_syllabu, description_syllabu, keywords_syllabu, pathimg_syllabus, state_syllabu
 , dateupdate_syllabu from syllabus
-where state_syllabu = 'A' and courses_id_course =1
+where state_syllabu = 'A' and courses_id_course =1;
 
 select *
 from topics
-where state_topic = 'A' and syllabus_id_syllabu =1
+where state_topic = 'A' and syllabus_id_syllabu =1;
