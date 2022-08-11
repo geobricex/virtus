f91f8ecf03c241aea2fb3a0fd4677756c4511168
ANTHONY PACHAY <anthony.pachay2017@uteq.edu.ec>
2022-08-11 15:20:35 -0500

    peque

--- virtus/src/app/pages/questionnaire/questionnaire.component.ts
+++ virtus/src/app/pages/questionnaire/questionnaire.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {BreadcrumbService} from "../../app.breadcrumb.service";
 
+declare var Artyom: any;
+
 @Component({
   selector: 'app-questionnaire',
   templateUrl: './questionnaire.component.html',
@@ -10,6 +12,34 @@ export class QuestionnaireComponent implements OnInit {
 
   valRadio: string;
 
+  private artyom: any = new Artyom();
+
+  public listQuestions: any = [
+    {
+      "question": "¿Cual es el nombre del periferico preferido al momento de interactuar con la computadora?",
+      "type": "optionSimple",
+      "points": 5,
+      "resolve": false,
+      "options": [
+        {
+          "content": "Mouse",
+          "trueResponse": false,
+          "response": true
+        },
+        {
+          "content": "Impresora",
+          "trueResponse": false,
+          "response": true
+        },
+        {
+          "content": "teclado",
+          "trueResponse": false,
+          "response": false
+        }
+      ]
+    }
+  ];
+
   constructor(private breadcrumbService: BreadcrumbService) {
     this.breadcrumbService.setItems([
       {label: 'Cuestionario', routerLink: ['/app/questionnaire']}
@@ -17,6 +47,16 @@ export class QuestionnaireComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  totalPuntos():number[]{
+    let total: number = 0;
+    let resueltas: number = 0;
+    for (let ind = 0; ind < this.listQuestions.length; ind++){
+      total+= this.listQuestions[ind].points;
+      resueltas+=1;
+    }
+    return [total, resueltas, this.listQuestions.length];
   }
 
 }
