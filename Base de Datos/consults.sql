2e6d32298514ce0c4cbaad9c4394ba19bfb7c405
geobricex <69307530+geobricex@users.noreply.github.com>
2022-08-21 02:43:03 -0500

    sum Quantity Question

--- Base de Datos/consults.sql
+++ Base de Datos/consults.sql
@@ -13,6 +13,24 @@ select * from answers
 inner join questions q on answers.questions_id_question = q.id_question
 inner join question_category qc on q.question_category_id_questioncategory = qc.id_questioncategory;
 
+SELECT id_evaluation,
+       name_evaluation,
+       description_evaluation,
+       datereg_evaluation,
+       dateupdate_evaluation,
+       time_evaluation,
+       timeminutes_evaluation,
+       topics_id_topic,
+       state_evaluation,
+       type_evaluation,
+       opportunities_evaluation,
+       order_category,
+       sum(eqc.number_question) as numberquestion_evaluation
+FROM evaluations
+         INNER JOIN evaluation_question_category eqc on evaluations.id_evaluation = eqc.evaluations_id_evaluation
+WHERE topics_id_topic=1
+group by id_evaluation
+
 
 
 
