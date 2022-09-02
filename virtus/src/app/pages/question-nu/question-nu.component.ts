b4e2dde740bf3d7ff16b3cdbc6d8c6baf4b7db41
Dúval Carvajal <duval.carvajal2017@uteq.edu.ec>
2022-09-02 01:13:20 -0500

    tipos de preguntas
    tipos de preguntas

--- virtus/src/app/pages/question-nu/question-nu.component.ts
+++ virtus/src/app/pages/question-nu/question-nu.component.ts
@@ -18,6 +18,7 @@ export class QuestionNuComponent implements OnInit {
   idEvaluation: string | null = "";
   typeQuestion: number;
   levelQuestion: number = 1;
+  textComplete: string | null;
 
   literales: any [];
 
@@ -28,6 +29,12 @@ export class QuestionNuComponent implements OnInit {
 
   valRadio: string;
   options: any [];
+
+  frmPhoto = new FormGroup({
+    firstName: new FormControl()
+  });
+  urlimageupload: any;
+  tmpFile: any;
 
   // ESTRUCTURA PARA LAS RESPUESTAS DE LAS PREGUNTAS
   structure: any [];
@@ -122,6 +129,66 @@ export class QuestionNuComponent implements OnInit {
     }
   }
 
+  // metodos para la estructura de relcionar
+  addNewAlternativeRel() {
+    if (this.structure.length > 5) {
+      alert("no puedes agregar mas items");
+      return;
+    }
+
+    this.structure.push({
+      "rightSide": "Texto de prueba",
+      "resourse_rightSide": "",
+      "leftSide": "Texto de prueba",
+      "resourse_leftSide": ""
+    })
+  }
+
+  // metodos para la estructura de puzzle
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      console.log(file);
+
+      var reader = new FileReader();
+      //this.imagePath = files;
+      reader.readAsDataURL(file);
+      reader.onload = (_event) => {
+        this.urlimageupload = reader.result;
+      }
+
+      const objectURL = URL.createObjectURL(file);
+      this.urlimageupload = objectURL;
+      console.log(objectURL)
+      this.tmpFile = file;
+      this.frmPhoto.patchValue({
+        field: file
+      });
+    }
+  }
+
+  // metodos para completar
+  addOption() {
+    let text_description = document.getElementsByName("descriptionText");
+    console.log(this.structure[0].description_question);
+    this.insertAtCursor(text_description[0], "[ingresa aqui la palabra correcta]");
+  }
+
+  addDistractor() {
+    this.structure[0].options.push({
+      "option": "",
+      "resource": ""
+    });
+  }
+
+  insertAtCursor(myField: any, myValue: string) {
+    var startPos = myField.selectionStart;
+    var endPos = myField.selectionEnd;
+    myField.value = myField.value.substring(0, startPos)
+      + myValue
+      + myField.value.substring(endPos, myField.value.length);
+  }
+
   deleteAlternative(index: number) {
     console.log(index)
     this.structure.splice(index, 1);
@@ -147,6 +214,30 @@ export class QuestionNuComponent implements OnInit {
         "correct": "No",
         "resource": ""
       })
+    } else if (this.typeQuestion === 5) {
+      this.structure.push({
+        "opcion": "TEXTO DE PRUEBA"
+      });
+    } else if (this.typeQuestion === 6) {
+      this.structure.push(
+        {
+          "rightSide": "Texto de prueba",
+          "resourse_rightSide": "",
+          "leftSide": "Texto de prueba",
+          "resourse_leftSide": ""
+        }
+      );
+    } else if (this.typeQuestion === 7) {
+      this.structure.push({
+        "resource": "",
+        "piece_questionarie": 0
+      });
+    } else if (this.typeQuestion === 4) {
+      this.structure.push({
+        "description_question": "texto de prueba",
+        "description_question_R": "texto de prueba",
+        "options": []
+      });
     }
   }
 
