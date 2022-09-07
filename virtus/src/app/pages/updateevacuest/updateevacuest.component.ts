3137e00c6050169077306eae5ea1c9d1c5923a16
geobricex <69307530+geobricex@users.noreply.github.com>
2022-09-06 20:53:12 -0500

    preview Question initial

--- virtus/src/app/pages/updateevacuest/updateevacuest.component.ts
+++ virtus/src/app/pages/updateevacuest/updateevacuest.component.ts
@@ -21,13 +21,19 @@ export class UpdateevacuestComponent implements OnInit {
   globalUrl: string = "";
   typeEvalutionform: any[];
   new_question_dialog: boolean = false;
+  new_question_view_dialog: boolean = false;
   quantity_true: number;
   quantityQuestions: any[];
-
+  public tmpPuzzle: Puzzle;
   public evaluationObject: Evaluation;
   public questionObject: Questions;
 
   frmEvaliationCuestionary: FormGroup;
+  public alphabet: string[] = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z"
+  ];
 
   constructor(
     private _route: ActivatedRoute,
@@ -232,4 +238,175 @@ export class UpdateevacuestComponent implements OnInit {
     return resp;
   }
 
+  public palabra: any = {
+    press: -1,
+    released: -1
+  };
+
+  clickUp(val: number): void {
+    console.log(" set", val);
+    if (val !== undefined && this.questionObject.answers_[0].complete_parts![val] !== "space") {
+      if (this.palabra.release === val) {
+        this.palabra.release = -1;
+      } else {
+        this.palabra.release = val;
+        console.log("poner", this.alphabet[this.palabra.press], "en ", this.palabra.release);
+        this.questionObject.answers_[0].complete_parts![this.palabra.release] = this.alphabet[this.palabra.press];
+        this.palabra.press = -1;
+        this.palabra.release = -1;
+      }
+    }
+  }
+
+  clickDown(val: number): void {
+    console.log("key", val, this.palabra.press === val);
+    if (val !== undefined) {
+      if (this.palabra.press === val) {
+        this.palabra.press = -1;
+      } else {
+        this.palabra.press = val;
+        this.palabra.release = -1;
+      }
+    }
+  }
+
+  showLetra(letra: string, indice: number): string {
+    if (letra === ' ')
+      return 'space';
+    if (this.questionObject.answers_[0].complete_parts![indice] == undefined)
+      return 'void';
+    else {
+      return this.questionObject.answers_[0].complete_parts![indice];
+    }
+  }
+
+  previewQuestion(question: Questions) {
+    this.new_question_view_dialog = true;
+
+    this.questionObject = question;
+    console.log(this.questionObject);
+    console.log(this.questionObject.answers_[0].options_answer);
+
+  }
+}
+
+class Puzzle {
+
+  private maxSizeImg: number = 25;
+  private dimens: number = 3;
+
+  public arrayImagePuzzle: string[][];
+  public arrayPositionPuzzle: number[][];
+
+  // public objImagePuzzleuzzle: string;
+
+  public puzzleControls: any = {
+    complete: false,
+    pressed: {x: -1, y: -1},
+    released: {x: -1, y: -1}
+  };
+
+  crearPuzzle(cantidad: number, url: string): void {
+    this.dimens = Math.sqrt(cantidad);
+
+    let maxSizeImg = this.maxSizeImg * this.dimens;
+    this.arrayImagePuzzle = new Array<string[]>(cantidad);
+    this.arrayPositionPuzzle = new Array<number[]>(cantidad);
+    let mecanvas = document.createElement("canvas") as HTMLCanvasElement;
+    //let mecanvas = document.getElementById("tmpImagenCanvas") as HTMLCanvasElement;
+    console.log(mecanvas);
+    mecanvas.width = maxSizeImg;
+    mecanvas.height = maxSizeImg;
+    let ctx = mecanvas.getContext('2d')!;
+    let local_this = this;
+    let img = new Image();
+    img.onload = function () {
+      img.width = maxSizeImg;
+      img.height = maxSizeImg;
+
+      ctx.drawImage(img, 0, 0, maxSizeImg, maxSizeImg);
+      let ind = 0;
+      for (let y = 0; y < local_this.dimens; y++) {
+        local_this.arrayImagePuzzle[y] = new Array<string>(local_this.dimens);
+        local_this.arrayPositionPuzzle[y] = new Array<number>(local_this.dimens);
+        for (let x = 0; x < local_this.dimens; x++) {
+          let imgData = ctx.getImageData(x * local_this.maxSizeImg, y * local_this.maxSizeImg,
+            (x * local_this.maxSizeImg) + local_this.maxSizeImg,
+            (y * local_this.maxSizeImg) + local_this.maxSizeImg);
+          let mincanvas = document.createElement("canvas") as HTMLCanvasElement;
+          mincanvas.width = local_this.maxSizeImg;
+          mincanvas.height = local_this.maxSizeImg;
+          let minctx = mincanvas.getContext('2d')!;
+          minctx.putImageData(imgData, 0, 0);
+
+          console.log("base64: ", mincanvas.toDataURL());
+          local_this.arrayImagePuzzle[y][x] = mincanvas.toDataURL();
+          local_this.arrayPositionPuzzle[y][x] = ind++;
+        }
+      }
+      /*let imgData = ctx.getImageData(0, 0, local_this.maxSizeImg, local_this.maxSizeImg);
+      let mincanvas = document.createElement("canvas") as HTMLCanvasElement;
+      mincanvas.width = local_this.maxSizeImg;
+      mincanvas.height = local_this.maxSizeImg;
+      let minctx = mincanvas.getContext('2d')!;
+      minctx.putImageData(imgData, 0, 0);
+
+      console.log("base64: ", mincanvas.toDataURL());
+      local_this.objImagePuzzle = mincanvas.toDataURL();*/
+    };
+    img.crossOrigin = "Anonymous";
+    img.src = url;
+  }
+
+  mover(x: number, y: number) {
+    if (!this.puzzleControls.complete) {
+      this.puzzleControls.pressed = {x: x, y: y};
+      this.puzzleControls.complete = true;
+    } else {
+      this.puzzleControls.released = {x: x, y: y};
+
+      let auxInd = this.arrayPositionPuzzle[this.puzzleControls.released.y][this.puzzleControls.released.x];
+      this.arrayPositionPuzzle[this.puzzleControls.released.y][this.puzzleControls.released.x] =
+        this.arrayPositionPuzzle[this.puzzleControls.pressed.y][this.puzzleControls.pressed.x];
+      this.arrayPositionPuzzle[this.puzzleControls.pressed.y][this.puzzleControls.pressed.x] = auxInd;
+
+      let auxbase64 = this.arrayImagePuzzle[this.puzzleControls.released.y][this.puzzleControls.released.x];
+      this.arrayImagePuzzle[this.puzzleControls.released.y][this.puzzleControls.released.x] =
+        this.arrayImagePuzzle[this.puzzleControls.pressed.y][this.puzzleControls.pressed.x];
+      this.arrayImagePuzzle[this.puzzleControls.pressed.y][this.puzzleControls.pressed.x] = auxbase64;
+
+      //hacer truco de cambio
+      this.puzzleControls.pressed = {x: -1, y: -1};
+      this.puzzleControls.released = {x: -1, y: -1};
+      this.puzzleControls.complete = false;
+    }
+  }
+
+  isActive(x: number, y: number) {
+    //if(!this.puzzleControls.complete){
+    return (this.puzzleControls.pressed.x === x && this.puzzleControls.pressed.y === y);
+    //(this.puzzleControls.rel.x === x && this.puzzleControls.pressed.y === y)
+    /*}
+    return false;*/
+  }
+
+  desordenar(unArray: any[]): any[] {
+    let t = unArray.sort(function (a, b) {
+      return (Math.random() - 0.5)
+    });
+    return [...t];
+  }
+
+  comprobarResultado(): number[] {
+    let ind = 0, success = 0;
+    for (let y = 0; y < this.dimens; y++) {
+      for (let x = 0; x < this.dimens; x++) {
+        if (this.arrayPositionPuzzle[y][x] == ind) {
+          success++; //fichas en el lugar correcto
+        }
+        ind++;
+      }
+    }
+    return [success, this.dimens * this.dimens];
+  }
 }
