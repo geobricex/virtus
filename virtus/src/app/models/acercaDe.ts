36519f874e5e0972116761bdcf973410a0023df9
Dúval Carvajal <duval.carvajal2017@uteq.edu.ec>
2022-09-08 14:39:41 -0500

    acerca de
    acerca de virtus

---
+++ virtus/src/app/models/acercaDe.ts
@@ -0,0 +1,20 @@
+export interface Authorities {
+  image: string;
+  name: string;
+  lastname: string;
+  email: string;
+  number_phone: string;
+  cargo: string;
+}
+
+export interface Development {
+  image: string;
+  name: string;
+  lastname: string;
+  email: string;
+  github: string;
+  facebook: string;
+  instagram: string;
+  linkedin: string;
+  cargo: string;
+}
--- virtus/src/app/pages/acercade/acercade.component.ts
+++ virtus/src/app/pages/acercade/acercade.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Development} from "../../models/acercaDe";
+import {BreadcrumbService} from "../../app.breadcrumb.service";
 
 @Component({
   selector: 'app-acercade',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercadeComponent implements OnInit {
 
-  constructor() { }
+  developments = [] as Development[];
+
+  constructor(
+    private breadcrumbService: BreadcrumbService
+  ) {
+    this.breadcrumbService.setItems([
+      {label: '', routerLink: ['/app']},
+      {label: 'Acerca de', routerLink: ['/app/course']}
+    ]);
+  }
 
   ngOnInit(): void {
+    this.developments = [
+      {name: "Geovanny José", lastname: "Brito Casanova", cargo: "Backend", facebook: "", github: "",image: "brito.png", email: "", instagram: "", linkedin: ""},
+      {name: "Dúval Ricardo", lastname: "Carvajal Suárez", cargo: "Frontend", facebook: "", github: "",image: "duval.jpg", email: "", instagram: "", linkedin: ""},
+      {name: "Anthony Abraham", lastname: "Pachay Espinoza", cargo: "Infraestructura", facebook: "", github: "",image: "pachay.jpg", email: "", instagram: "", linkedin: ""},
+      {name: "Valeria Dayanna", lastname: "Torres Lindao", cargo: "Documentación", facebook: "", github: "",image: "valeria.jpeg", email: "", instagram: "", linkedin: ""}
+    ];
   }
 
 }
